1b4d57cdae6fc74c0d3b39dc62e5d8e25dae0a51
xaviguasch <xaviguasch@gmail.com>
2020-10-13 23:00:47 +0200

    solved useEffect: HTTP requests EXTRA CREDIT 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,27 +2,13 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
 import {
   fetchPokemon,
   PokemonInfoFallback,
   PokemonForm,
   PokemonDataView,
 } from '../pokemon'
-
-class ErrorBoundary extends React.Component {
-  state = {error: null}
-  static getDerivedStateFromError(error) {
-    return {error}
-  }
-  render() {
-    const {error} = this.state
-    if (error) {
-      return <this.props.FallbackComponent error={error} />
-    }
-
-    return this.props.children
-  }
-}
 
 function PokemonInfo({pokemonName}) {
   const [state, setState] = React.useState({
